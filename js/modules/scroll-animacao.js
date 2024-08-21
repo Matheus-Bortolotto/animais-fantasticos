1827e8485629a2bfbae241fdffe168ab24d1a5d3
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-08-20 22:35:42 -0300

    ajustando função scroll

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,5 +1,6 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
+
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
@@ -7,9 +8,9 @@ export default function initAnimacaoScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) section.classList.add("ativo");
-        else if (section.classList.contains("ativo")) {
-          section.classList.remove("ativo");
+
+        if (isSectionVisible && !section.classList.contains("ativo")) {
+          section.classList.add("ativo");
         }
       });
     }
